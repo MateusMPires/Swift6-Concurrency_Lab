4b5fb6e6250047da51cf044257fb13533593fea7
Mateus Pires <mateusmp.df@gmail.com>
2026-03-13 18:36:12 -0300

    Initial Commit

---
+++ Concurrency_Lab/Concurrency_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_LabApp.swift
+//  Concurrency_Lab
+//
+//  Created by Mateus Martins Pires on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Concurrency_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
